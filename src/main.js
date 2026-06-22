a11d36d0131909af31dd1cafdde563b94453c432
levoxtrip <levoxtrip@gmail.com>
2026-06-22 13:55:31 +0200

    style adapted

--- src/main.js
+++ src/main.js
@@ -77,7 +77,7 @@ function markCompleted() {
 function renderStickerPage() {
   document.body.style.setProperty('--page-bg', `url('${CONFIG.page1Bg}')`);
   app.innerHTML = `
-    <div class="page page-sticker">
+    <div class="page page-sticker" style="background-image:url('${CONFIG.page1Bg}')">`;
       <div class="location-label" id="locationLabel">
         <img src="${CONFIG.locationIcon}" alt="">
         <span id="locationText">${CONFIG.cities[0]}</span>
@@ -276,7 +276,7 @@ function renderThanksPage() {
   player = null;
   document.body.style.setProperty('--page-bg', `url('${CONFIG.page2Bg}')`);
   app.innerHTML = `
-    <div class="page page-thanks">
+    <div class="page page-thanks" style="background-image:url('${CONFIG.page2Bg}')">`;
       
     </div>
   `;
